ef53b7bb8320130aec894c06091ada9cc256936f
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-09-22 19:00:33 +0300

    Update search.py
    assignment completed

--- Lists/search.py
+++ Lists/search.py
@@ -3,7 +3,11 @@ def search(lines, word):
     for line in range(1, lines + 1):
         my_str = input()
         str_list.append(my_str)
-    for s in str_list:
-        if word not in s:
-            str_list.remove(s)
-        
+    print(str_list)
+    for index in range(len(str_list) - 1, -1, -1):
+        if word not in str_list[index]:
+            str_list.remove(str_list[index])
+    print(str_list)
+
+
+search(lines=int(input()), word=input())
