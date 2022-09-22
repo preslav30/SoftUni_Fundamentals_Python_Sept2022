3a8ba60cff160d3b02fe9a925e0047f2a3cd005d
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-09-22 19:32:33 +0300

    Update numbers_filter.py

--- Lists/numbers_filter.py
+++ Lists/numbers_filter.py
@@ -1,1 +1,31 @@
-def 
+
+
+def numbers_filter(lines):
+    nums = []
+    new_list = []
+    for line in range(1, lines + 1):
+        num = int(input())
+        nums.append(num)
+    command = input()
+
+    if command == "even":
+        for number in nums:
+            if number % 2 == 0:
+                new_list.append(number)
+    if command == "odd":
+        for number in nums:
+            if number % 2 != 0:
+                new_list.append(number)
+
+    if command == "negative":
+        for number in nums:
+            if number < 0:
+                new_list.append(number)
+
+    if command == "positive":
+        for number in nums:
+            if number >  0:
+                new_list.append(number)
+    return new_list
+
+print(numbers_filter(lines=int(input())))
