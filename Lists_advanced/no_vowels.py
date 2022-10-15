a8f72827a5c11adcad3613760a2d678fb6e42b1c
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-10-15 19:49:10 +0300

    Update no_vowels.py

--- Lists_advanced/no_vowels.py
+++ Lists_advanced/no_vowels.py
@@ -1,8 +1,8 @@
 def vowels(txt):
     vowels_list = ['a', 'o', 'u', 'e', 'i', 'A', 'O', 'U', 'E', 'I']
-    txt_filtered = [char for char in txt if char not in vowels_list]
-    return txt_filtered
+    txt_filtered = list(filter(lambda char: char not in vowels_list, txt))
+    return "".join(txt_filtered)
 
 
-text = input().split()
+text = input()
 print(vowels(text))
