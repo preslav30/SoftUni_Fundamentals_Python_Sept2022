0e41dbca16b8d87f70f876f550e85aaf742a2762
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-10-16 16:24:52 +0300

    Create office_chairs.py

---
+++ Lists_advanced/office_chairs.py
@@ -0,0 +1,9 @@
+def office(rooms, chairs_per_room_plus_visitors):
+    chairs = len(chairs_per_room_plus_visitors[0])
+    people = int(chairs_per_room_plus_visitors[1])
+
+
+
+number_of_rooms = int(input())
+chairs_plus_visitors = input().split()
+
