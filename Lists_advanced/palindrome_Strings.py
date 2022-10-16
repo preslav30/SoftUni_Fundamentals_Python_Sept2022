f09bdaf91516a5724a727d9aa614e8f3ba643b52
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-10-16 11:39:57 +0300

    Create palindrome_Strings.py

---
+++ Lists_advanced/palindrome_Strings.py
@@ -0,0 +1,13 @@
+def palindrome_count(my_str):
+    pals = list(filter(lambda pal: pal == pal[::-1], my_str))
+    return pals
+def palindrome_occurrence(pal):
+    count = 
+
+
+
+
+
+words = input().split()
+palin = input()
+print(palindrome(words))
