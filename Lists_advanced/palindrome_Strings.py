7737026b3258008a064b48d78c4cbe61315bf81b
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-10-16 11:44:02 +0300

    Update palindrome_Strings.py

--- Lists_advanced/palindrome_Strings.py
+++ Lists_advanced/palindrome_Strings.py
@@ -1,13 +1,15 @@
 def palindrome_count(my_str):
     pals = list(filter(lambda pal: pal == pal[::-1], my_str))
     return pals
-def palindrome_occurrence(pal):
-    count = 
 
 
-
+def palindrome_occurrence(my_str, pal):
+    count = my_str.count(pal)
+    return count
 
 
 words = input().split()
 palin = input()
-print(palindrome(words))
+
+print(palindrome_count(words))
+print(f"Found palindrome {palindrome_occurrence(words, palin)} times")
