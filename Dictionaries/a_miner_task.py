e99e2cb9e27ef62e0b4e89818d4cda39f1799f14
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-11-06 21:03:02 +0200

    Create a_miner_task.py

---
+++ Dictionaries/a_miner_task.py
@@ -0,0 +1,12 @@
+resources = {}
+while True:
+    resource = input()
+    if resource == "stop":
+        break
+    quantity = int(input())
+    if resource in resources:
+        resources[resource] += quantity
+    else:
+        resources[resource] = quantity
+for key, value in resources.items():
+    print(f"{key} -> {value}")
