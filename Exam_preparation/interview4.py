8e3e12f2680af984bdf5f9fa0575f5866f53e505
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-11-24 14:38:11 +0200

    initial_commit

---
+++ Exam_preparation/interview4.py
@@ -0,0 +1,13 @@
+words = "cat, horse, sweater, schoolbus, shark, cat, cat, horse, shark, shark, shark"
+words = words.split(", ")
+
+# occurrences = dict((i, words.count(i)) for i in words)
+# for word, occurrence in occurrences.items():
+#     print(f"{word} |" + "*" * occurrence)
+
+
+occurrences = {}
+for i in words:
+    occurrences[i] = words.count(i)
+for word, occurrence in occurrences.items():
+    print(f"{word} |" + "*" * occurrence)
