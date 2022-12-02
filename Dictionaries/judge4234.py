c259e13bb14790408eb4dddc8473b24027db70a7
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-12-02 20:10:02 +0200

    in progress

--- Dictionaries/judge4234.py
+++ Dictionaries/judge4234.py
@@ -1,9 +1,23 @@
+contests = {}
+total_points = {}
 while True:
     data = input()
     if data == "no more time":
         break
     data = data.split(" -> ")
-    contest = data[0]
-    username = data[1]
+    contest = data[1]
+    username = data[0]
     points = int(data[2])
-    
+    if contest not in contests.keys():
+        contests[contest] = []
+    if username in contests[contest]:
+        total_points[username][1] += points
+        if points > contests[contest][1]:
+            contests[contest][1] = points
+    else:
+        total_points[username] = [contest, points]
+        contests[contest].append({username: points})
+
+print(contests)
+# for contest, un_points in contests.items():
+#     print(f"")
